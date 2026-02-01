f0e73e8991e1bd32d93e2215273688deb6057db0
Blake Park <blake.r.park@gmail.com>
2026-02-01 04:30:07 +0000

    mk4 engine

--- src/bitboard/mod.rs
+++ src/bitboard/mod.rs
@@ -2,16 +2,19 @@
 //!
 //! This module provides the foundation for all board state and piece manipulation.
 
-pub mod types;
 pub mod attacks;
 pub mod magic;
+pub mod position;
+pub mod position;
+pub mod types;
 
 pub use self::prelude::*;
 
 pub mod prelude {
-    pub use super::types::*;
     pub use super::attacks::*;
     pub use super::magic::*;
+    pub use super::position::*;
+    pub use super::types::*;
 }
 
 pub mod lib {
